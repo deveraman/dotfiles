383794a96e19e7d2c54f337a5c40a2957040ca29
maranix <ramanverma4183@gmail.com>
2024-06-30 00:55:09 +0530

    I HATE IT HERE!!!!!

--- dots/.config/nvim/lua/config/server/zls.lua
+++ dots/.config/nvim/lua/config/server/zls.lua
@@ -1,5 +1,5 @@
 return {
-    cmd = "zls",
+    cmd = { "zls" },
     filetypes = { "zig", "zir" },
     root_dir = require("lspconfig.util").root_pattern("zls.json", "build.zig", ".git"),
     single_file_support = true,
