4228e571be2a4e90a0598c39b4d98241b58fd360
maranix <ramanverma4183@gmail.com>
2024-07-10 00:01:36 +0530

    neovim: fix configs not loading properly

--- dots/.config/nvim/lua/util/init.lua
+++ dots/.config/nvim/lua/util/init.lua
@@ -1,7 +1,7 @@
 local function set_server_config(name)
     local config_path = 'config.server.' .. name
 
-    return require('lspconfig')[name].setup(require(config_path))
+    return require('lspconfig')[name].setup(require(config_path).default_config)
 end
 
 return {
