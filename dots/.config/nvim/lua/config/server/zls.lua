b18d43dc0777934b14bfca9e603e76e5cb0d0303
maranix <ramanverma4183@gmail.com>
2024-08-17 00:31:58 +0530

    nvim: update zls

--- dots/.config/nvim/lua/config/server/zls.lua
+++ dots/.config/nvim/lua/config/server/zls.lua
@@ -1,20 +1,23 @@
 local util = require 'lspconfig.util'
 
+-- Currently autoformat on save is broken and hangs up the IDE
+vim.g.zig_fmt_autosave = 0
+
 return {
-  default_config = {
-    cmd = { 'zls' },
-    filetypes = { 'zig', 'zir' },
-    root_dir = util.root_pattern('zls.json', 'build.zig', '.git'),
-    single_file_support = true,
-  },
-  docs = {
-    description = [[
+    default_config = {
+        cmd = { 'zls' },
+        filetypes = { 'zig', 'zir' },
+        root_dir = util.root_pattern('zls.json', 'build.zig', '.git'),
+        single_file_support = true,
+    },
+    docs = {
+        description = [[
 https://github.com/zigtools/zls
 
 Zig LSP implementation + Zig Language Server
         ]],
-    default_config = {
-      root_dir = [[util.root_pattern("zls.json", "build.zig", ".git")]],
+        default_config = {
+            root_dir = [[util.root_pattern("zls.json", "build.zig", ".git")]],
+        },
     },
-  },
 }
